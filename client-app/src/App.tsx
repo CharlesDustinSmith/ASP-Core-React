d9db5360bec0601b4898d8d140823c9cef64f4d5
Charles Dustin Smith <cdsmith@mun.ca>
2023-07-31 18:26:40 -0230

    Playing around with axios, useState, and useEffect

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { ducks } from './Demo';
 import DuckItem from './DuckItem';
+import axios from 'axios';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  // The '[]' was used to add some depenedency making it only call once
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activites/")
+    .then(response => {
+      console.log(response);
+      setActivities(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {ducks.map( duck => (
-          <DuckItem duck={ duck } key={duck.name}></DuckItem>
-        ))}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {activities.map((activity: any) => (
+            <li key={activity.id}>
+              {activity.title}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// {ducks.map( duck => (
+//   <DuckItem duck={ duck } key={duck.name}></DuckItem>
+// ))}
+// <p>
+//   Edit <code>src/App.tsx</code> and save to reload!!!
+// </p>
+// <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
+//   Learn React
+// </a>
