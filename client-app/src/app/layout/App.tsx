b0d41a358d5cde4c6d4f8737f50fee8033b2ff86
Charles Dustin Smith <cdsmith@mun.ca>
2023-08-02 19:20:00 -0230

    Added an interface activities in the react application

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Button, Header, List } from 'semantic-ui-react';
+import { IActivity } from '../models/activity';
+
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
   // The '[]' was used to add some depenedency making it only call once
   useEffect(() => {
-    axios.get("http://localhost:5000/api/activities")
+    axios.get<IActivity[]>("http://localhost:5000/api/activities")
     .then(response => {
       setActivities(response.data);
     });
@@ -17,7 +19,7 @@ function App() {
     <div>
         <Header as='h2' icon='users' content='Reactivities'></Header>
         <List>
-          {activities.map((activity: any) => (
+          {activities.map((activity) => (
             <List.Item key={activity.id}>
               {activity.title}
             </List.Item>
