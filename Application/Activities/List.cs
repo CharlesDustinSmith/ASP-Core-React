cae3207073f20a9c243e622ae7d4c1016415e36c
Charles Dustin Smith <cdsmith@mun.ca>
2023-08-02 18:20:32 -0230

    Playing around with cancellation token will remove them after this commit

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -2,6 +2,7 @@ using MediatR;
 using Domain;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Activities
 {
@@ -12,14 +13,31 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
+            private readonly ILogger<List> _logger;
 
-            public Handler(DataContext context)
+            public Handler(DataContext context, ILogger<List> logger)
             {
                 this._context = context;
+                this._logger = logger;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
+                try
+                {
+                    for(var i = 0; i < 10; i++ )
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000, cancellationToken);
+                        _logger.LogInformation($"Task {i} has completed");
+                    }
+                }
+                catch (System.Exception)
+                {
+                    _logger.LogInformation("Task was cancelled");
+                    throw;
+                }
+
                 return await _context.Activities.ToListAsync();
             }
         }
