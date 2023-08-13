01263e070dc2b32c1f213ffe250e207581154f56
Charles Dustin Smith <cdsmith@mun.ca>
2023-08-12 22:29:54 -0230

    Axios types using templates

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,19 +1,20 @@
 import axios, { AxiosResponse } from 'axios';
+import { IActivity } from '../models/activity';
 
 // Setting up base url so that I wouldn't have to type it all the time.
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
-const responseBody = (response: AxiosResponse) => response.data;
+const responseBody = <T> (response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get: (url: string) => axios.get(url).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-    del: (url: string) => axios.delete(url).then(responseBody)
+    get: <T> (url: string) => axios.get<T>(url).then(responseBody),
+    post: <T> (url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T> (url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    del: <T> (url: string) => axios.delete<T>(url).then(responseBody)
 }
 
 const Activities = {
-    list: () => requests.get('/activities')
+    list: () => requests.get<IActivity[]>('/activities')
 }
 
 const agent = {
